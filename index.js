d4cea4c2245a47c621cf5c911574d28b3e2c6db3
AustinKocab <austinko1128@gmail.com>
2024-06-14 19:45:32 -0400

    jsonDataCheck

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-function fetchJSONData() {
-    fetch("./menuTest.json")
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error
-                    (`HTTP error! Status: ${res.status}`);
-            }
-            return res.json();
-        })
-        .then((data) =>
-            console.log(data))
-        .catch((error) =>
-            console.error("Unable to fetch data:", error));
+import { fetchJSONData } from "./fetchMethod";
+
+function createButtons() {
+    var container = document.getElementById("menuDiv");
+    var data = fetchJSONData("./Menus/menus.json");
+    console.log(data);
+    container.appendChild();
 }
