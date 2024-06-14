8fb962ee747a969544a00685d6df83970eba510b
AustinKocab <austinko1128@gmail.com>
2024-06-14 19:22:28 -0400

    reset

--- devTransition.js
+++
@@ -1,8 +0,0 @@
-var password = "southside2255"
-
-function submit() {
-    var textBox = document.getElementById("input-box");
-    if (textBox.value == password) {
-        window.location.href="./configs/config.html"
-    }
-}
