595f0ccfdb453ec0163db68b9ea00479b4825287
AustinKocab <austinko1128@gmail.com>
2024-06-14 20:36:22 -0400

    real shit

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ async function createButtons() {
     const data = await response.json();
     console.log(data);
 
-    console.log(movies);
-
     data.menus.forEach(menu => {
         var button = document.createElement("button");
         button.innerText = menu.title;
